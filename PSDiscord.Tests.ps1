c71a7d417c048ffca39207b924d4b120b28c9ca1
PrzemyslawKlys <pklys@evotec.pl>
2019-01-14 22:27:01 +0100

    Fix for tests to download PSSharedGoods

--- PSDiscord.Tests.ps1
+++ PSDiscord.Tests.ps1
@@ -11,10 +11,10 @@ if ($Pester -eq $null -or ($Pester[0].Version.Major -le 4 -and $Pester[0].Versio
     Write-Warning "$ModuleName - Downloading Pester from PSGallery"
     Install-Module -Name Pester -Repository PSGallery -Force -SkipPublisherCheck -Scope CurrentUser
 }
-#if ((Get-Module -ListAvailable PSSharedGoods) -eq $null) {
-#    Write-Warning "$ModuleName - Downloading PSSharedGoods from PSGallery"
-#    Install-Module -Name PSSharedGoods -Repository PSGallery -Force -Scope CurrentUser
-#}
+if ((Get-Module -ListAvailable PSSharedGoods) -eq $null) {
+    Write-Warning "$ModuleName - Downloading PSSharedGoods from PSGallery"
+    Install-Module -Name PSSharedGoods -Repository PSGallery -Force -Scope CurrentUser
+}
 
 $result = Invoke-Pester -Script @{ Path = "$($PSScriptRoot)\Tests"; Parameters = @{ DiscordURL = $DiscordURL }; } -EnableExit
 
