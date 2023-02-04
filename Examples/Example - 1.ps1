ab520ecd3306648629bea2ccb3d3fd57e3d28718
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-02-04 19:48:16 +0100

    Update example

--- Examples/Example - 1.ps1
+++ Examples/Example - 1.ps1
@@ -1,5 +1,6 @@
 Import-Module .\PSDiscord.psd1
 
-$DiscordUrl = '....'
+$DiscordUrl = ''
 
-Send-DiscordMessage -WebHookUrl $DiscordUrl -Content 'My pester tests - Starting up' -AvatarName 'Pester Tester' -AvatarUrl "https://raw.githubusercontent.com/EvotecIT/PSTeams/master/Links/Asset%20130.png"
+Send-DiscordMessage -WebHookUrl $DiscordUrl -Content 'My pester tests - Starting up @everyone' -AvatarName 'Pester Tester' -AvatarUrl "https://raw.githubusercontent.com/EvotecIT/PSTeams/master/Links/Asset%20130.png"
+Send-DiscordMessage -WebHookUrl $DiscordUrl -Content 'My pester tests - Starting up @PrzemyslawKlys' -AvatarName 'Pester Tester' -AvatarUrl "https://raw.githubusercontent.com/EvotecIT/PSTeams/master/Links/Asset%20130.png"
